b36c0899e7331aa6cb742170cc660b84d75de1de
Ethan Li <lietk12@gmail.com>
2018-10-09 18:40:50 +0000

    Fixed bug with PID file caused by using tee.

--- cancel_startup.sh
+++ cancel_startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 PIDFILENAME="startup.pid"
-kill -SIGINT `cat "${DIRNAME}/${PIDFILENAME}"`
-rm "${DIRNAME}/${PIDFILENAME}"
+kill -SIGINT `cat "${DIRNAME}/${PIDFILENAME}"` && rm "${DIRNAME}/${PIDFILENAME}"
--- startup.sh
+++ startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 PIDFILENAME="startup.pid"
-echo $DIRNAME
-/usr/bin/python3 "${DIRNAME}/sticktest.py" -c | tee "${DIRNAME}/startup.log" &
+/usr/bin/python3 "${DIRNAME}/sticktest.py" -c > "${DIRNAME}/startup.log" &
 PID=$!
 echo $PID > "${DIRNAME}/${PIDFILENAME}"

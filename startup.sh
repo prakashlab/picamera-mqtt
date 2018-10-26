ba911da52d568f47b87975728253ff57b1a37856
Ethan Li <lietk12@gmail.com>
2018-10-26 11:02:59 +0000

    Removed cancellation of startup breathe.

--- startup.sh
+++ startup.sh
@@ -4,7 +4,6 @@ PIDFILENAME="startup.pid"
 STARTWAIT=5
 
 cd "${DIRNAME}"
-/bin/bash "cancel_startup.sh" # Quit the script started in startup_breathe.sh
 /usr/bin/python3 -m intervention_client.mqtt_illumination > "${DIRNAME}/startup.log" &
 PID=$!
 echo $PID > "${PIDFILENAME}"

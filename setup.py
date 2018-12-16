7afa39c73c4a7a0bd65d0c2608c9792452291208
Ethan Li <lietk12@gmail.com>
2018-12-15 20:25:09 -0800

    Added initial setup.py for PyPI availability.

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='picamera-mqtt',
+    version='0.0.1',
+    author='Ethan Li',
+    author_email='lietk12@gmail.com',
+    description=(
+        'MQTT-based control and image capture with local and remote networked '
+        'Raspberry Pi cameras.'
+    ),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ethanjli/picamera-mqtt',
+    packages=setuptools.find_packages(),
+    license='BSD 3-Clause License',
+    install_requires=[
+        'paho-mqtt',
+        'pillow'
+    ],
+    extras_require={
+        'picamera_client': ['picamera'],
+        'mock_camera_client': ['numpy']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Multimedia :: Graphics :: Capture :: Digital Camera'
+    ]
+)
